08d6c76ea6177c910ff9e79d5a08a980f0cbad21
YashWankar <yashwankar25@gmail.com>
2020-12-18 21:25:33 +0530

    Day3 solution with learnings

--- hacko30doc/Day3sol.java
+++ hacko30doc/Day3sol.java
@@ -13,16 +13,20 @@ public class Day3sol {
     }
 }
 
-/*
- * Learnings are : long n = 461012; System.out.format("%d%n", n); // -->
- * "461012" System.out.format("%08d%n", n); // --> "00461012"
- * System.out.format("%+8d%n", n); // --> " +461012" System.out.format("%,8d%n",
- * n); // --> " 461,012" System.out.format("%+,8d%n%n", n); // --> "+461,012"
- * 
- * double pi = Math.PI;
- * 
- * System.out.format("%f%n", pi); // --> "3.141593" System.out.format("%.3f%n",
- * pi); // --> "3.142" System.out.format("%10.3f%n", pi); // --> "     3.142"
- * System.out.format("%-10.3f%n", pi); // --> "3.142"
- * System.out.format(Locale.FRANCE, "%-10.4f%n%n", pi); // --> "3,1416"
- */
+/*Learnings are :
+      long n = 461012;
+      System.out.format("%d%n", n);      //  -->  "461012"
+      System.out.format("%08d%n", n);    //  -->  "00461012"
+      System.out.format("%+8d%n", n);    //  -->  " +461012"
+      System.out.format("%,8d%n", n);    // -->  " 461,012"
+      System.out.format("%+,8d%n%n", n); //  -->  "+461,012"
+      
+      double pi = Math.PI;
+
+      System.out.format("%f%n", pi);       // -->  "3.141593"
+      System.out.format("%.3f%n", pi);     // -->  "3.142"
+      System.out.format("%10.3f%n", pi);   // -->  "     3.142"
+      System.out.format("%-10.3f%n", pi);  // -->  "3.142"
+      System.out.format(Locale.FRANCE,
+                        "%-10.4f%n%n", pi); // -->  "3,1416"
+*/
