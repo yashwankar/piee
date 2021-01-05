fd5617182e11444f8c36f661fcf6cf5e98d236dd
YashWankar <yashwankar25@gmail.com>
2021-01-05 19:23:32 +0530

    day21 generics types

---
+++ hacko30doc/Day21Sol.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+class Printer <T> {
+    void printArray(T list[]){
+        for(T element:list)
+        System.out.println(element);
+    }
+}
+
+public class Day21Sol {
+    
+    public static void main(String args[]){
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        Integer[] intArray = new Integer[n];
+        for (int i = 0; i < n; i++) {
+            intArray[i] = scanner.nextInt();
+        }
+
+        n = scanner.nextInt();
+        String[] stringArray = new String[n];
+        for (int i = 0; i < n; i++) {
+            stringArray[i] = scanner.next();
+        }
+        
+        Printer<Integer> intPrinter = new Printer<Integer>();
+        Printer<String> stringPrinter = new Printer<String>();
+        intPrinter.printArray( intArray  );
+        stringPrinter.printArray( stringArray );
+        if(Printer.class.getDeclaredMethods().length > 1){
+            System.out.println("The Printer class should only have 1 method named printArray.");
+        }
+    } 
+}
+
+/*
+Learnings:
+About the generics types<> and its working.
+*/
