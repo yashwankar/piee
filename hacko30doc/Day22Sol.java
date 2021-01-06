b4115838eebce108fc07e8bd53dd178cda519376
YashWankar <yashwankar25@gmail.com>
2021-01-06 18:01:47 +0530

    day22 height of binary search tree

---
+++ hacko30doc/Day22Sol.java
@@ -0,0 +1,50 @@
+import java.util.*;
+import java.io.*;
+class Node{
+    Node left,right;
+    int data;
+    Node(int data){
+        this.data=data;
+        left=right=null;
+    }
+}
+class Day22Sol{
+	public static int getHeight(Node root){
+        if (root==null) return -1;
+        return 1+Math.max(getHeight(root.left),getHeight(root.right));
+    }
+    public static Node insert(Node root,int data){
+        if(root==null){
+            return new Node(data);
+        }
+        else{
+            Node cur;
+            if(data<=root.data){
+                cur=insert(root.left,data);
+                root.left=cur;
+            }
+            else{
+                cur=insert(root.right,data);
+                root.right=cur;
+            }
+            return root;
+        }
+    }
+	 public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int T=sc.nextInt();
+        Node root=null;
+        while(T-->0){
+            int data=sc.nextInt();
+            root=insert(root,data);
+        }
+        int height=getHeight(root);
+        System.out.println(height);
+    }
+}
+
+/*
+Learnings:
+The traversal in BST
+The height of a binary search tree can be found using the recursive relation.
+*/
