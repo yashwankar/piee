d02e3c17801aa0a7bec6be02eba3adc39cee9c54
YashWankar <yashwankar25@gmail.com>
2021-01-22 20:29:37 +0530

    nutanix bubbleSort

---
+++ nutanix/BubbleSort.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class BubbleSort {
+    static void countSwaps(int[] a) {
+        int count=0;
+        int len = a.length - 1;
+        for(int i=0; i<len; i++){
+            for(int j=0; j<len; j++){
+                if(a[j]>a[j+1]){
+                    int temp = a[j];
+                    a[j] = a[j+1];
+                    a[j+1] = temp;
+                    count++;
+                }
+            }
+        }
+        System.out.println("Array is sorted in " + count + " swaps.");
+        System.out.println("First Element: " + a[0]);
+        System.out.println("Last Element: " + a[len]);
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] a = new int[n];
+
+        String[] aItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int aItem = Integer.parseInt(aItems[i]);
+            a[i] = aItem;
+        }
+
+        countSwaps(a);
+        scanner.close();
+    }
+}
